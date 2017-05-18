282cf8729277e88004f87cfc96fc2db358ef79e8
Cale Tilford <combinekidd@gmail.com>
2017-05-18 01:35:03 +0100

    Add US topojson example instead

---
+++ colour.js
@@ -0,0 +1,13 @@
+function getPolygonArea(points) {
+  var sum = 0.0;
+  var length = points.length;
+  if (length < 3) {
+    return sum;
+  }
+  points.forEach(function(d1, i1) {
+    i2 = (i1 + 1) % length;
+    d2 = points[i2];
+    sum += (d2[1] * d1[0]) - (d1[1] * d2[0]);
+  });
+  return sum / 2;
+}
